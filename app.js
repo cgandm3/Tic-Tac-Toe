77ff75820dec982d82dd02c0397dcba6beb0b9c9
Steve F Soter <cgandm3@me.com>
2015-05-14 18:30:25 -0700

    working version of js

--- app.js
+++ app.js
@@ -1,26 +1,108 @@
-var score = 0;
-var boxes = document.getElementsByClassName('box');
-console.log(boxes);
-var player = true;
+var scoreX = 0;
+var scoreO = 0;
+var box = document.getElementsByClassName("box");
+//console.log(box);
 
-inputMoves();
+var winningCombos = [
+["1","2","3"], 
+["4","5","6"], 
+["7","8","9"], 
+["1","4","7"],
+["2","5","8"],
+["3","6","9"],
+["1","5","9"],
+["3","5","7"]];
 
-function randomNumber(){
-	return Math.floor(Math.random() * 9);
+var allmoves = [];
+
+var moves = {
+  "X": [],
+  "O": []
 }
 
+var which = "X"
+
+window.addEventListener("load", function() {
+  inputMoves();
+});
+
+
 function inputMoves() {
-	for(var i = 0; i < boxes.length; i++){
-		boxes[i].addEventListener('click', function(){
-			if(player === true & this.innerHTML === ""){
-				this.innerHTML = "X"; player = false;
-			} else if(player === false & this.innerHTML === ""){
-				this.innerHTML ="O"; player = true;
-			} else {
-				alert("Choose an empty space");
-			}
-			
-		}
-	);
-	};
-};
+//listen for a click on any box and push the box number into
+//an array depending on whose turn it is and an array for all moves
+//    console.log("start inputMoves");
+    for(var i = 0; i < box.length; i++) {
+//        console.log("loop called");
+        box[i].addEventListener('click', function(m) {
+            this.innerHTML = which;
+            moves[which].push(m.target.id);
+ //           console.log(moves);
+            allmoves.push(m.target.id);
+            // console.log(allmoves);
+            // console.log(moves.X);
+            // console.log(moves.O);
+            if (
+              hasWinningCombo(moves[which], winningCombos) == true
+              ){
+                 alert(which + " wins");
+                // console.log("waiting");
+                 clearBoard(); 
+                 return;
+               }  
+            else if (
+                  allmoves.length === 9
+                 ){ 
+                    alert("it's a tie!");
+                    //console.log("waiting");
+                    clearBoard();
+                    return;
+                  } 
+            if (
+              which === "X"
+              ){
+                which = "O"
+            } else { 
+                which = "X"
+            } 
+      //console.log(which);                     
+    });
+   } 
+  }
+
+function hasWinningCombo(playerMoves, winningCombos) {
+//check the moves made by x or o against the winningcombos array
+  for(var i = 0; i < winningCombos.length; i++) {
+      if (
+          playerMoves.indexOf(winningCombos[i][0]) >= 0
+          && playerMoves.indexOf(winningCombos[i][1]) >= 0
+          && playerMoves.indexOf(winningCombos[i][2]) >= 0
+          ){
+              return true;
+      }
+  }
+  return false;
+}
+
+// function clearBoardClick() {
+//  
+//   document.getElementById("clear").addEventListener('click', function() {
+//         for (var i = 0; i < box.length; i++) 
+//         {
+//           box[i].innerHTML= "";
+//         };
+//       });
+// }
+
+function clearBoard() {
+//initialze all arrays and set which variable to X
+        allmoves = [];
+        moves.X = [];
+        moves.O = [];
+        which = "X"
+        //console.log(moves.X);
+        //console.log(moves.O);
+        for (var i = 0; i < box.length; i++) 
+        {
+            box[i].innerHTML= "";
+        };
+      };
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 angular
-	.module('TicTacToeApp', []);
+	.module('TicTacToeApp', ['firebase']);
 
 	// Step 2 - create a module in main.js and use the ng-app to link it to the body (or html)
